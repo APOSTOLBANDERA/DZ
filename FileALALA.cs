ca6ae85425e080c4a130f8e27559bbe5c9485d66
APOSTOLBANDERA <dendenisov2704@gmail.com>
2022-09-24 14:45:35 +0300

    Start

---
+++ FileALALA.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    public class FileALALA
+    {
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public int Size { get; set; }
+        public DateTime Date { get; set; }
+
+
+        public FileALALA(string name, string extension, int size, DateTime date)
+        {
+            Name = name;
+            Extension = extension;
+            Size = size;
+            Date = date;
+        }
+
+
+        public string NextSize()
+        {
+            string[] arr = new string[5] { "b", "kb", "mb", "gb", "tb" };
+            int i;
+            for (i = 0; Size > 1024;)
+            {
+                i++;
+                Size = Size / 1024;
+            }
+            return ($"{Size} {arr[i]}");
+        }
+
+        public override string ToString()
+        {
+            return $"Name:{Name}, Extension:{Extension}, Size:{Size}, Date:{Date}";
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp9;
+
+
+FileALALA[] files = new FileALALA[]
+{
+    new FileALALA("ala", "txt", 1024, new DateTime(2022, 09, 17)),
+    new FileALALA("alaxua", "jpg", 1000, DateTime.Now),
+    new FileALALA("alaua", "txt", 5569, new DateTime(2022, 08, 17)),
+    new FileALALA("alal", "png", 124747124, new DateTime(2022, 07, 17)),
+    new FileALALA("alakar", "rar", 134413, new DateTime(2022, 06, 17)),
+};
+//0
+Console.WriteLine(files[4].NextSize());
+Console.WriteLine();
+//1
+foreach(FileALALA file in files)
+{
+    if(file.Extension == "txt")
+    {
+        Console.WriteLine(file);
+    }
+}
+Console.WriteLine();
+//2
+foreach (FileALALA file in files)
+{
+    if (file.Name.Contains("ua"))
+    {
+        Console.WriteLine(file);
+    }
+}
+Console.WriteLine();
+//3
+foreach (FileALALA file in files)
+{
+    if (file.Date < new DateTime(2022, 09, 17))
+    {
+        Console.WriteLine(file);
+    }
+}
